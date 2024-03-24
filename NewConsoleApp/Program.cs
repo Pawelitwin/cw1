ed42c96ecaea7c441a8b911d39b46d25bff6357c
s19502 <s19502@pjwstk.edu.pl>
2024-03-24 15:50:51 +0100

    Modyfikacja kolejna

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -18,9 +18,9 @@ class Program
         }
 
         int sum = 0;
-        foreach (int num in array)
+        foreach (int nuum in array)
         {
-            sum += num;
+            sum += nuum;
         }
 
         return (double)sum / array.Length;

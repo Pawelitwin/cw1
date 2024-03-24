fa82b31a3665ddf577e7b9f863507e7c1fd367d0
s19502 <s19502@pjwstk.edu.pl>
2024-03-24 15:32:33 +0100

    Modyfikacja 4

--- NewConsoleApp/Program.cs
+++ NewConsoleApp/Program.cs
@@ -1,7 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 10, 20, 30, 40, 50 };
+        double average = CalculateAverage(numbers);
+        Console.WriteLine("Średnia wynosi: " + average);
+    }
+
+    static double CalculateAverage(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            throw new ArgumentException("Tablica jest pusta lub null.");
+        }
+
+        int sum = 0;
+        foreach (int num in array)
+        {
+            sum += num;
+        }
+
+        return (double)sum / array.Length;
+    }
+
 
